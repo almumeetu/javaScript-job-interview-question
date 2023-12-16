8d580a97d71c51a18fd8c427a9e27409203166af
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-12-17 00:23:39 +0600

    practice function js

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-function saikatShihab() {
-    console.log("My Name Is Saikat");
-}
+function saikatShihab(name, firs) {
+    console.log(firs + name);
+};
 
-console.log("My Name Is Jasim");
+saikatShihab("Saikat", "My Name Is ");
+saikatShihab("Shihab", "My Brother Name is ");
+saikatShihab("Raton", "My Friends Name Is ");
+
+
+let y = myFunction(4, 1)
+
+let z = myFunction(5, 9)
+
+
+
+
+
+function myFunction(a, b) {
+    return a * b;
+};
+
+let x = myFunction(5, 3)
+
+console.log(x);
+
+console.log(y);
+
+console.log(z);

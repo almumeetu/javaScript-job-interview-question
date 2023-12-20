94a1e3f7779f18e23b0262f23255d82086989392
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-12-20 22:38:46 +0600

    toExponential() methods

--- main.js
+++ main.js
@@ -33,4 +33,12 @@ saikatShihab("Raton", "My Friends Name Is ");
 
 let a = myFunction(4, 1)
 
-let b = myFunction(5, 9)
+let b = myFunction(5, 9);
+
+
+let c = 9.656;
+document.getElementById("demo").innerHTML =
+  c.toExponential() + "<br>" + 
+  c.toExponential(2) + "<br>" + 
+  c.toExponential(4) + "<br>" + 
+  c.toExponential(6);
